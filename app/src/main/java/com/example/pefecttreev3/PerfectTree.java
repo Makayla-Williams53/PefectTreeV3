efdcc6340144c5cd897b04c00d5092070c6f5042
Makayla <makaylawms0426@gmail.com>
2023-04-05 21:12:03 -0700

    Have created the second activity which will display the actual tree.

---
+++ app/src/main/java/com/example/pefecttreev3/PerfectTree.java
@@ -0,0 +1,14 @@
+package com.example.pefecttreev3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PerfectTree extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_perfect_tree);
+    }
+}
